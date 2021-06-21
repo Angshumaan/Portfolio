c1cf5c8dff607a79adbfa283780f117b3632fcc7
= <=>
2021-06-21 21:44:11 +0530

    staticfiles

--- angshu/settings.py
+++ angshu/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [
     'angshu/static',
 ]
